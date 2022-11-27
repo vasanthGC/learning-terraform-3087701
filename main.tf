84007d9227a381d5673c76a4222ebf9e514ad2af
vasanthGC <107972188+vasanthGC@users.noreply.github.com>
2022-11-27 23:14:09 +0530

    update azs

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-1a", "us-west-1b"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true

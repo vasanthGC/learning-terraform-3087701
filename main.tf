6cab6f8e3473d65b7ef15c5137f09f29d7fbf16d
vasanthGC <107972188+vasanthGC@users.noreply.github.com>
2022-11-27 23:00:47 +0530

    create dev vpc

--- main.tf
+++ main.tf
@@ -17,6 +17,24 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "3.18.1"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -32,7 +50,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.2"
   name = "blog"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]

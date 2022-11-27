05f2b78bb19c61d18cdaf3b3b1c5c2e1825f35ba
vasanthGC <107972188+vasanthGC@users.noreply.github.com>
2022-11-27 23:01:20 +0530

    delete default vpc from config file

--- main.tf
+++ main.tf
@@ -13,9 +13,6 @@ data "aws_ami" "app_ami" {
 
   owners = ["979382823631"] # Bitnami
 }
-data "aws_vpc" "default" {
-  default = true
-}
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
